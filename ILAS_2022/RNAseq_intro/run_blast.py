885cc67f906992d19b9a9f989a4693780ed8aee5
qqep685d <qqep685d@gmail.com>
2022-06-24 12:37:00 +0900

    update run_blast

--- ILAS_2022/RNAseq_intro/run_blast.py
+++ ILAS_2022/RNAseq_intro/run_blast.py
@@ -1,10 +1,13 @@
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 
-def run_blast(prog, db, seq):
-  with NCBIWWW.qblast(program=prog, database=db, sequence=seq) as handle:
-    recs = NCBIXML.parse(handle)
-    for rec in recs:
-      for alignment in rec.alignments:
-        for hsp in alignment.hsps:
-          return alignment.accession, alignment.hit_def
+def run_blast(seq, prog="blastx", db="swissprot"):
+  with NCBIWWW.qblast(prog, db, seq) as handle:
+    recs = NCBIXML.parse(handle):
+    if recs:
+      for rec in recs:
+        for alignment in rec.alignments:
+          for hsp in alignment.hsps:
+            return alignment.accession, alignment.hit_def
+    else:
+      return None, None
