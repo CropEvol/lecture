aaf7000f2e2d19c7f0d74393d3d90d0d0bb8d95b
qqep685d <qqep685d@gmail.com>
2022-06-24 22:50:37 +0900

    fix bugs in run_blast.py

--- ILAS_2022/RNAseq_intro/run_blast.py
+++ ILAS_2022/RNAseq_intro/run_blast.py
@@ -2,12 +2,14 @@ from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 
 def run_blast(seq, prog="blastx", db="swissprot"):
-  with NCBIWWW.qblast(prog, db, seq) as handle:
-    recs = NCBIXML.parse(handle)
-    if recs:
-      for rec in recs:
-        for alignment in rec.alignments:
-          for hsp in alignment.hsps:
-            return alignment.accession, alignment.hit_def
-    else:
-      return None, None
+  # BLAST検索を実行
+  handle = NCBIWWW.qblast(prog, db, seq)
+  # 検索結果の読み込み
+  recs = NCBIXML.parse(handle)
+  for rec in recs:
+    # 検索結果1以上のとき、ベストスコアの結果のみ取得
+    for alignment in rec.alignments:
+      for hsp in alignment.hsps:
+        return alignment.accession, alignment.hit_def
+    # 検索結果0のとき
+    return None, None
