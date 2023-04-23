75cd427d986de2d9ed5dc9462081bbbb4917ec94
qqep685d <qqep685d@gmail.com>
2023-04-23 17:10:30 +0900

    update install_softwares_p2.sh

--- FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
+++ FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
@@ -7,3 +7,5 @@ cd ${base_dir}/tools
 wget -q https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -O - | tar -zxf -
 ln -sf ${base_dir}/tools/ncbi-blast-2.13.0+/bin/* /usr/bin/
 printf "completed.\n"
+
+touch QUE.fasta
