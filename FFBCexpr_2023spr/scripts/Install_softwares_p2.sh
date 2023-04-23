974bad1987a86e486a05b8d539ac32c1777c75eb
qqep685d <qqep685d@gmail.com>
2023-04-23 21:09:16 +0900

    Install_softwares_p2.sh

--- FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
+++ FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
@@ -8,4 +8,11 @@ wget -q https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-
 ln -sf ${base_dir}/tools/ncbi-blast-2.13.0+/bin/* /usr/bin/
 printf "completed.\n"
 
-touch QUE.fasta
+cd ${base_dir}/
+cat /dev/null > QUERY.fasta
+
+printf "Installing samtools ..."
+cd ${base_dir}/tools
+wget -q https://www.dropbox.com/s/8ko897pr47kup0k/samtools-1.17.tar.gz?dl=0 -O - | tar -zxf -
+ln -sf ${base_dir}/tools/samtools-1.17/samtools /usr/bin/
+printf "completed.\n"
