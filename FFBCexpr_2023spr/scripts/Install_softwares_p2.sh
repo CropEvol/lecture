71b69cb1cbd8c8dc4feeee339bd2c200a5764279
Toshiyuki Sakai <toshi6661024@gmail.com>
2023-05-02 17:41:54 +0900

    Update Install_softwares_p2.sh

--- FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
+++ FFBCexpr_2023spr/scripts/Install_softwares_p2.sh
@@ -4,7 +4,7 @@ mkdir -p ${base_dir}/tools
 ## BLAST+ (local blast tool)
 printf "Installing BLAST+ ..."
 cd ${base_dir}/tools
-wget -q https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -O - | tar -zxf -
+wget -q https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.13.0/ncbi-blast-2.13.0+-x64-linux.tar.gz -O - | tar -zxf -
 ln -sf ${base_dir}/tools/ncbi-blast-2.13.0+/bin/* /usr/bin/
 printf "completed.\n"
 
