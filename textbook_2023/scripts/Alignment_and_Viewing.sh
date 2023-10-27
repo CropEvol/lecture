2445145f6776e771faa1ec0c4d6855002c63959d
qqep685d <qqep685d@gmail.com>
2023-10-27 22:31:19 +0900

    update Alignment_and_Viewing.sh

--- textbook_2023/scripts/Alignment_and_Viewing.sh
+++ textbook_2023/scripts/Alignment_and_Viewing.sh
@@ -3,10 +3,10 @@ CONTIG=$1
 OUT_NAME=$2
 
 bwa index $CONTIG
-bwa mem $CONTIG $@ > $OUT_NAME.sam
+bwa mem $CONTIG ${@:3} > $OUT_NAME.sam
 samtools sort -O bam $OUT_NAME.sam > $OUT_NAME.bam
 rm -f $OUT_NAME.sam
 
 samtools faidx $CONTIG
 
-python show_alignment.py $CONTIG $OUT_NAME.bam
+python tools/show_alignment.py $CONTIG $OUT_NAME.bam
