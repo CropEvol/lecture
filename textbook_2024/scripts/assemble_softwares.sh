4f3dcb401514a912727d3e7c5e2983bcd9924bad
qqep685d <qqep685d@gmail.com>
2024-10-02 16:37:45 +0900

    update textbook_2024/scripts/assemble_softwares.sh

--- textbook_2024/scripts/assemble_softwares.sh
+++ textbook_2024/scripts/assemble_softwares.sh
@@ -3,16 +3,16 @@ TOOL_DIR="/content/tools"
 mkdir -p $TOOL_DIR
 cd $TOOL_DIR
 
+
 ### 参考：GoogleDriveのファイルをwgetでダウンロードする方法
 ### https://qiita.com/cv_carnavi/items/1fc8272da135a36598a2
+### https://qiita.com/IsHYuhi/items/e4afc0163019343d9664
 
-# Platanus-1.2.4 from GoogleDrive
+## Platanus-1.2.4 from GoogleDrive
 FILE_ID="1UOH7yZvCbbEWLgJWBOm9U2rCaxXwJ50J";
 FILE_NAME="Platanus-1.2.4.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/platanus /usr/bin/
@@ -21,53 +21,43 @@ ln -sf $TOOL_DIR/$FILE_BASE/platanus /usr/bin/
 FILE_ID="183cxJNMY8BiG2LGpvbOnv8fAcU6Piv0r";
 FILE_NAME="Flye-2.9.2.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/flye /usr/bin/
 
 ## bwa-0.7.17 from GoogleDrive
-FILE_ID="10k9fkfIdXuiJ1D2VMt10-Q2ws0APH3mq";
+FILE_ID="10k9fkfIdXuiJ1D2VMt10-Q2ws0APH3mq"; #共有リンクの発行で確認可能
 FILE_NAME="bwa-0.7.17.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/bwa /usr/bin/
 
-## samtools-1.17 from GoogleDrive
+## samtools-1.21 from GoogleDrive
 FILE_ID="1hKPNXzGgPG_f2CvK87Be4sVx0bLkXXYE";
 FILE_NAME="samtools-1.21.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/samtools /usr/bin/
 
-## bcftools-1.17 from GoogleDrive
+## bcftools-1.21 from GoogleDrive
 FILE_ID="1--rhCPb655FeAUokOGRcvwj7O0doTs84";
 FILE_NAME="bcftools-1.21.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/bcftools /usr/bin/
 
-## htslib-1.17 from GoogleDrive
+## htslib-1.21 from GoogleDrive
 FILE_ID="1-3HEsOQrygyigtQ1M9KKuztYn48CP7W4";
 FILE_NAME="htslib-1.21.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/tabix /usr/bin/
@@ -76,22 +66,11 @@ ln -sf $TOOL_DIR/$FILE_BASE/bgzip /usr/bin/
 ## SeqKit v2.5.1 from GoogleDrive
 FILE_ID="1JTiW78S0K4H_ag42wy5ojPLWKSR7V3_E";
 FILE_NAME="seqkit_linux_amd64.tar.gz";
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/seqkit /usr/bin/
-
-# ## MUMmer3.23 from GoogleDrive
-# FILE_ID="1JGTuRdf8-zdKnZ4XR_WxsOrvGmE4B14L";
-# FILE_NAME="MUMmer3.23.tar.gz";
-# CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-# wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-# rm -f /tmp/cookies.txt
-# tar -zxf $FILE_NAME
-# rm -f $FILE_NAME
-# ln -sf $TOOL_DIR/nucmer /usr/bin/
 
 ## igv-notebook-0.3.1 from Github
 pip install -q igv-notebook==0.3.1
