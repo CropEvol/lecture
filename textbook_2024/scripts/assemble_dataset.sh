1416d18b24302ce63cb8db216d42731fc9dfbbde
qqep685d <qqep685d@gmail.com>
2024-10-16 10:49:02 +0900

    update textbook/scripts/assemble_dataset.sh

--- textbook_2024/scripts/assemble_dataset.sh
+++ textbook_2024/scripts/assemble_dataset.sh
@@ -5,18 +5,18 @@ cd $DOWNLOAD_DIR
 
 FILE_NAME="illumina_read1.fastq.gz"
 FILE_NAME2=`basename -s ".gz" $FILE_NAME`
-wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2023/dataset/$FILE_NAME
+wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2024/dataset/$FILE_NAME
 gzip -dc $FILE_NAME > $FILE_NAME2
 rm -f $FILE_NAME
 
 FILE_NAME="illumina_read2.fastq.gz"
 FILE_NAME2=`basename -s ".gz" $FILE_NAME`
-wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2023/dataset/$FILE_NAME
+wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2024/dataset/$FILE_NAME
 gzip -dc $FILE_NAME > $FILE_NAME2
 rm -f $FILE_NAME
 
 FILE_NAME="nanopore.fastq.gz"
 FILE_NAME2=`basename -s ".gz" $FILE_NAME`
-wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2023/dataset/$FILE_NAME
+wget -q -O $FILE_NAME https://github.com/CropEvol/lecture/raw/master/textbook_2024/dataset/$FILE_NAME
 gzip -dc $FILE_NAME > $FILE_NAME2
 rm -f $FILE_NAME
