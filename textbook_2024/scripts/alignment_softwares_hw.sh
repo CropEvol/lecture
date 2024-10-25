6593689d7dd6991835b8965069971de78cdea815
qqep685d <qqep685d@gmail.com>
2024-10-26 07:21:33 +0900

    update alignment_softwares_hw.sh

--- textbook_2024/scripts/alignment_softwares_hw.sh
+++ textbook_2024/scripts/alignment_softwares_hw.sh
@@ -3,24 +3,20 @@ TOOL_DIR="/content/tools"
 mkdir -p $TOOL_DIR
 cd $TOOL_DIR
 
-## samtools-1.17 from GoogleDrive
-FILE_ID="1fGKLLtAaJmTHAc4uGRtb_0r-HB2M6yQv";
-FILE_NAME="samtools-1.17.tar.gz";
+## samtools-1.21 from GoogleDrive
+FILE_ID="1hKPNXzGgPG_f2CvK87Be4sVx0bLkXXYE";
+FILE_NAME="samtools-1.21.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/samtools /usr/bin/
 
-## htslib-1.17 from GoogleDrive
-FILE_ID="1CCEDZzrpVR5WYpWkvbVyQZ4UHlerLXNO";
-FILE_NAME="htslib-1.17.tar.gz";
+## htslib-1.21 from GoogleDrive
+FILE_ID="1-3HEsOQrygyigtQ1M9KKuztYn48CP7W4";
+FILE_NAME="htslib-1.21.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
-CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://drive.google.com/uc?export=download&id=$FILE_ID" -O- | sed -En 's/.*confirm=([0-9A-Za-z_]+).*/\1/p');
-wget -q --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $FILE_NAME;
-rm -f /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/tabix /usr/bin/
@@ -28,4 +24,4 @@ ln -sf $TOOL_DIR/$FILE_BASE/bgzip /usr/bin/
 
 ## igv-notebook-0.3.1
 pip install -q igv-notebook==0.3.1
-wget -q -O ../igv_prep.py           https://github.com/CropEvol/lecture/raw/master/textbook_2022/scripts/igv_prep.py
+wget -q -O ../igv_prep.py           https://github.com/CropEvol/lecture/raw/master/textbook_2024/scripts/igv_prep.py
