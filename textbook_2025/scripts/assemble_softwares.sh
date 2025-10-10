6dff658364b54080115e20a3d0fc3dc76f393d9c
qqep685d <qqep685d@gmail.com>
2025-10-10 17:04:21 +0900

    update software file

--- textbook_2025/scripts/assemble_softwares.sh
+++ textbook_2025/scripts/assemble_softwares.sh
@@ -6,16 +6,16 @@ wget -q -O Alignment_mm2.sh https://github.com/CropEvol/lecture/raw/master/textb
 wget -q -O Polishing.sh     https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/Polishing.sh
 
 pip install -q igv-notebook==0.3.1
-wget -q -O ../igv_prep.py   https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/igv_prep.py
+wget -q -O igv_prep.py  https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/igv_prep.py
 
-apt install miniasm
-apt install spades
-apt install seqkit
-apt install bwa 
-apt install minimap2
-apt install samtools
-apt install bcftools
-apt install pilon
+apt install miniasm >/dev/null
+apt install spades >/dev/null
+apt install seqkit >/dev/null
+apt install bwa  >/dev/null
+apt install minimap2 >/dev/null
+apt install samtools >/dev/null
+apt install bcftools >/dev/null
+apt install pilon >/dev/null
 
 FILE_ID="14rI7Solj6KZh4YpnSSwUB_97ZgFQMFQB";
 FILE_NAME="Flye-2.9.6.tar.gz";
@@ -24,6 +24,8 @@ gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/bin/flye /usr/bin/
+
+echo '... completed'
 
 ### 参考：GoogleDriveのファイルをwgetでダウンロードする方法
 ### https://qiita.com/cv_carnavi/items/1fc8272da135a36598a2
