aa88255b9867587dc19c2d3402a376945495d887
qqep685d <qqep685d@gmail.com>
2025-10-31 13:50:35 +0900

    update assemble_softwares

--- textbook_2025/scripts/assemble_softwares.sh
+++ textbook_2025/scripts/assemble_softwares.sh
@@ -17,9 +17,9 @@ tar -zxf $FILE_NAME
 rm -f $FILE_NAME
 ln -sf $TOOL_DIR/$FILE_BASE/bin/spades.py /usr/bin/
 
-## Flye-2.9.2 from GoogleDrive
-FILE_ID="183cxJNMY8BiG2LGpvbOnv8fAcU6Piv0r";
-FILE_NAME="Flye-2.9.2.tar.gz";
+## Flye-2.9.6 from GoogleDrive
+FILE_ID="14rI7Solj6KZh4YpnSSwUB_97ZgFQMFQB";
+FILE_NAME="Flye-2.9.6.tar.gz";
 FILE_BASE=`basename -s ".tar.gz" $FILE_NAME`
 gdown https://drive.google.com/uc?id=$FILE_ID --no-cookies -O $FILE_NAME
 tar -zxf $FILE_NAME
@@ -69,6 +69,6 @@ pip install -q igv-notebook==0.3.1
 wget -q -O pilon-1.24.jar https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar
 
 ## pipelines
-wget -q -O ../Polishing.sh          https://github.com/CropEvol/lecture/raw/master/textbook_2024/scripts/Polishing_java.sh
-wget -q -O ../Alignment_mm2.sh      https://github.com/CropEvol/lecture/raw/master/textbook_2024/scripts/Alignment_mm2.sh
-wget -q -O ../igv_prep.py           https://github.com/CropEvol/lecture/raw/master/textbook_2024/scripts/igv_prep.py
+wget -q -O ../Polishing.sh          https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/Polishing_java.sh
+wget -q -O ../Alignment_mm2.sh      https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/Alignment_mm2.sh
+wget -q -O ../igv_prep.py           https://github.com/CropEvol/lecture/raw/master/textbook_2025/scripts/igv_prep.py
