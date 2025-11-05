a797b2e6a9eadafbc209bed16508165625da0d0e
qqep685d <qqep685d@gmail.com>
2025-11-05 12:48:48 +0900

    update README

--- textbook_2025/scripts/Flye_assemble.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-Flye='python /content/tools/Flye-2.9.6/bin/flye'
-MIN_OVL=1000
-OUT_DIR="/content/flye_output"
-
-READ=$1
-
-${Flye} --out-dir $OUT_DIR --min-overlap $MIN_OVL --scaffold --nano-raw $READ
--- textbook_2025/scripts/Platanus_assemble.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-OUT_DIR="/content/platanus_output"
-OUT_NAME="illumina"
-
-mkdir -p $OUT_DIR
-platanus assemble -o $OUT_DIR/$OUT_NAME -f "${@:1}"
